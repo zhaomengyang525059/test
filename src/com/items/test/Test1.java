5ad4f550bdd400f266cb1934a4baa1804e1e419e
zmy <acid8080@163.com>
2021-09-06 13:53:26 +0800

    second commit

--- src/com/items/test/Test1.java
+++ src/com/items/test/Test1.java
@@ -3,6 +3,7 @@ package com.items.test;
 public class Test1 {
     public static void main (String[] args){
         System.out.println("hello world!");
+        System.out.println("hello world2!");
 
     }
 }
