5cf4ba3853fd412d38c5863b2e15a7a158b89830
zmy <acid8080@163.com>
2021-09-06 13:53:44 +0800

    third commit

--- src/com/items/test/Test1.java
+++ src/com/items/test/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
     public static void main (String[] args){
         System.out.println("hello world!");
         System.out.println("hello world2!");
+        System.out.println("hello world3!");
 
     }
 }
